a18bd74c000e894aa32c1ec790de5ecf497a30d0
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-08-24 00:22:54 +0800

    Update connection

--- shortener/database.py
+++ shortener/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(os.environ["DB_CONNECTION"])
+engine = create_engine(os.environ["DATABASE_URL"])
 
 Base = declarative_base()
 
