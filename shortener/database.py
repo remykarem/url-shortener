849f32a478b57c2ea96ca677b96be6d66ba05ffa
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-08-24 00:14:24 +0800

    Update db connection

--- shortener/database.py
+++ shortener/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(
-    "postgresql+psycopg2://raimi:mysecretpassword@localhost/postgres")
+engine = create_engine(os.environ["DB_CONNECTION"])
 
 Base = declarative_base()
 
