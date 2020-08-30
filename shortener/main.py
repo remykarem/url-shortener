45b77c3d8ff77cd2ba7c00d62b6816384da38d22
raibosome <raimi.bkarim@gmail.com>
2020-08-30 15:12:02 +0800

    Add CORS middleware

--- shortener/main.py
+++ shortener/main.py
@@ -1,6 +1,10 @@
 from typing import List
 
 from fastapi import FastAPI, Depends
+from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 from sqlalchemy.orm import Session  # for typing
 
 from .database import engine, SessionLocal
@@ -11,6 +15,13 @@ from . import crud
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/public", StaticFiles(directory="public"), name="public")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 def get_db():
@@ -23,20 +34,23 @@ def get_db():
 
 @app.get("/")
 def index():
-    return {"Hello": "World"}
+    return FileResponse("public/index.html")
 
 
 @app.get("/urls/", response_model=List[schemas.UrlRead])
 def read_urls(db: Session = Depends(get_db)):
+    """For debugging"""
     urls = crud.get_urls(db)
     return urls
 
 
 @app.post("/urls/")
-def create_url(url: schemas.UrlBase, db: Session = Depends(get_db)):
-    return crud.create_url(db=db, url=url)
+def create_url(url: schemas.UrlCreate, db: Session = Depends(get_db)):
+    crud.create_url(db=db, url=url)
+    return {"status": 1}
 
 
 @app.get("/{short}", response_model=schemas.UrlRead)
 def get_url(short: str, db: Session = Depends(get_db)):
-    return crud.get_url(db=db, short=short)
+    url = crud.get_url(db=db, short=short)
+    return RedirectResponse(url=f"http://{url.link}")
