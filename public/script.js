69032310fcf176571f4d14666f05b97d64dec521
raibosome <raimi.bkarim@gmail.com>
2020-09-13 23:11:24 +0800

    Add copytoclipboard

--- public/script.js
+++ public/script.js
@@ -3,21 +3,42 @@ var app = new Vue({
     data: {
         link: 'tech.gov.sg',
         shortLink: '',
+        loading: false
     },
     methods: {
         shorten: function (event) {
             const vm = this;
+
+            vm.loading = true;
+            vm.shortLink = '';
+
             axios.post('/urls/', {
                 raw: this.link
             })
                 .then(function (response) {
                     console.log(response);
                     console.log(response.data.shortLink);
+                    vm.loading = false;
                     vm.shortLink = response.data.shortLink;
                 })
                 .catch(function (error) {
                     console.log(error)
                 })
+        },
+        copyToClipboard: function (event) {
+            var text = document.getElementById('short-link');
+            var range = document.createRange();
+            
+            window.getSelection().removeAllRanges();
+            range.selectNode(text);
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+            window.getSelection().removeAllRanges();
+
+            UIkit.notification({
+                message: "<center><small>Copied to clipboard</small></center>",
+                pos: "bottom-center"
+            });
         }
     }
 })
