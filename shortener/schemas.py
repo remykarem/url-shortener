dff717c341a200f12f524a8d479e2e5afb8d2aa8
raibosome <raimi.bkarim@gmail.com>
2020-09-14 21:18:55 +0800

    Update schemas

--- shortener/schemas.py
+++ shortener/schemas.py
@@ -1,10 +1,13 @@
 from typing import NewType, Tuple
 from pydantic import BaseModel
 
-UrlRaw = NewType("UrlRaw", str)
-UrlHash = NewType("UrlHash", str)
-UrlShort = NewType("UrlShort", str)
-UrlShortLink = NewType("UrlShortLink", str)
+UrlRaw = NewType("UrlRaw", str)  # google.com
+UrlHash = NewType("UrlHash", str)  # 542565888
+UrlShort = NewType("UrlShort", str)  # 542-565-888
+UrlShortLink = NewType("UrlShortLink", str)  # <appname>/542-565-888
+
+NUM_DIGITS = 9
+GROUPS = 3
 
 
 class UrlBase(BaseModel):
@@ -26,5 +29,11 @@ class UrlCreate(BaseModel):
 def hasher(url_raw: UrlRaw) -> Tuple[UrlHash, UrlShort]:
     url_hash = hash(url_raw)
     url_hash = str(abs(url_hash))[:9]
-    url_short = f"{url_hash[:3]}-{url_hash[3:6]}-{url_hash[6:9]}"
+    url_short = hash_to_short(url_hash)
     return url_hash, url_short
+
+
+def hash_to_short(url_hash: UrlHash) -> UrlShort:
+    arr = [url_hash[i*GROUPS:(i+1)*GROUPS]
+           for i in range(GROUPS)]
+    return "-".join(arr)
